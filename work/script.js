3c810ec5dbed11bb6d6520e3f19f77fa4bf2072f
vishalsolanki <vishalsolanki4880@gmail.com>
2025-03-03 10:12:32 +0530

    02-03-2025

--- work/script.js
+++ work/script.js
@@ -1,12 +1,20 @@
 // 1. find maximum number in array.
 
-function maximum(array) {
-    let maximum = array[0]
+// let array = [10,20,30,40,50,60];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > maximum) {
-            maximum = array[i];
-        }        
-    }
-    return maximum;
-}
+// function maximum(array) {
+//     let maximum = array[0]
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] > maximum) {
+//             maximum = array[i];
+//         }        
+//     }
+//     return maximum;
+// }
+
+// console.log(maximum(array));
+
+// 2. remove repateElement in array
+
+// transpose array 
