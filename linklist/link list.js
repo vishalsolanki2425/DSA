6f6673040f4bec0fd6e2244221c9c5a6315e6a07
vishalsolanki <vishalsolanki4880@gmail.com>
2025-03-08 10:30:24 +0530

    08-03-2025

---
+++ linklist/link list.js
@@ -0,0 +1,11 @@
+class node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+    class (single) {
+        constructor(data) {
+            this.data = data;
+        }
+    }
+}
