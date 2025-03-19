9db6ad0c6dc1eba0e765f7fd29872c77e23297e6
vishalsolanki <vishalsolanki4880@gmail.com>
2025-03-19 10:04:56 +0530

    19-03-2025

--- work/script.js
+++ work/script.js
@@ -18,3 +18,47 @@
 // 2. remove repateElement in array
 
 // transpose array 
+
+
+// let arr = [2, 45, 32, 45, 66, 77, 32]
+
+
+//   function len (arr, target) {
+//     for (let i =0; i < arr.length; i++){
+//         if(arr[i] == target){
+//             return i
+//         }
+//     }
+//     return -1
+
+//   }
+
+//   console.log(len(arr, (76)))
+
+
+
+let arr = [1, 2, 3, 4, 5, 6 ]
+
+function binary (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+
+    while(left <= right){
+        
+    let mid =  Math.floor((left + right) / 2)
+        if(arr[mid] == target){
+            return mid
+        }
+        else if(arr[mid] < target){
+            left = mid + 1
+        }
+        else if(arr[mid] > target){
+            right = mid - 1
+        }
+
+    }
+}
+
+console.log(binary(arr, (5)))
+
