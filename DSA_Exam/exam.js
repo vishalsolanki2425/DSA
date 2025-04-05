bfc47e7a2a3fade06897b2f16078735298ebe66b
vishalsolanki <vishalsolanki4880@gmail.com>
2025-04-05 10:52:54 +0530

    04-03-2025

--- DSA_Exam/exam.js
+++ DSA_Exam/exam.js
@@ -47,102 +47,189 @@
 
 // output :  [1, 2, 3, 4, 5, 6]
 
+
 /* =================================================================================== */
 
 // Q.3 WAP to implements Quick Sort using JavaScript.
 
-// function quickSort(arr, left = 0, right = arr.length - 1) {
-//     if (left < right) {
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
 
-//         let pivotIndex = partition(arr, left, right);
+        let pivotIndex = partition(arr, left, right);
 
-//         quickSort(arr, left, pivotIndex - 1);
-//         quickSort(arr, pivotIndex + 1, right);
-//     }
-//     return arr;
-// }
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
 
-// function partition(arr, left, right) {
+function partition(arr, left, right) {
 
-//     let pivot = arr[right];
-//     let i = left - 1;
+    let pivot = arr[right];
+    let i = left - 1;
 
-//     for (let j = left; j < right; j++) {
-//         if (arr[j] <= pivot) {
-//             i++;
-//             [arr[i], arr[j]] = [arr[j], arr[i]];
-//         }
-//     }
+    for (let j = left; j < right; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
 
-//     [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
-//     return i + 1;
-// }
+    [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+    return i + 1;
+}
 
-// let unsortedArray = [7, 2, 1, 6, 8, 5, 3, 4];
-// console.log("Unsorted Array:", unsortedArray);
+let unsortedArray = [7, 2, 1, 6, 8, 5, 3, 4];
+console.log("Unsorted Array:", unsortedArray);
 
-// let sortedArray = quickSort([...unsortedArray]);
-// console.log("Sorted Array:", sortedArray);
+let sortedArray = quickSort([...unsortedArray]);
+console.log("Sorted Array:", sortedArray);
+
 
 /* =================================================================================== */
 
 // Q.4 WAP to implements Create singly Linked list, adding a new Elements, and Delete node from singly linked list (Without any pre-defined function)
 
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
 
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//     }
+
+//     addEle(data) {
+//         let newNode = new Node(data);
+
+//         if (this.head === null) {
+//             this.head = newNode;
+//         } else {
+//             let cerr = this.head;
+//             while (cerr.next !== null) {
+//                 cerr = cerr.next;
+//             }
+//             cerr.next = newNode;
+//         }
+//     }
+
+//     deleteELe(value) {
+//         if (this.head === null) {
+//             console.log("List is empty");
+//             return;
+//         }
+
+//         if (this.head.data === value) {
+//             this.head = this.head.next;
+//             return;
+//         }
+
+//         let cerr = this.head;
+//         let prev = null;
+
+//         while (cerr !== null && cerr.data !== value) {
+//             prev = cerr;
+//             cerr = cerr.next;
+//         }
+
+//         if (cerr === null) {
+//             console.log("Node not found");
+//             return;
+//         }
+
+//         prev.next = cerr.next;
+//     }
+
+//     display() {
+//         let cerr = this.head;
+//         let result = "";
+
+//         while (cerr !== null) {
+//             result += cerr.data + " -> ";
+//             cerr = cerr.next;
+//         }
+
+//         result += "null";
+//         console.log("Linked List : ", result);
+//     }
+// }
+
+// let linked = new LinkedList();
+
+// linked.addEle(10);
+// linked.addEle(20);
+// linked.addEle(30);
+// linked.addEle(40);
+// linked.display();
+
+// linked.deleteELe(20);
+// linked.display(); 
+
+// linked.deleteELe(10);
+// linked.display();
+
+// Linked List: 10 -> 20 -> 30 -> 40 -> null
+// Linked List: 10 -> 30 -> 40 -> null
+// Linked List: 30 -> 40 -> null
 
 
 /* =================================================================================== */
 
 // Q.5 WAP to implement insert and remove element from Queue using JavaScript.
 
-class Queue {
-    constructor() {
-        this.arr = [];
-    }
+// class Queue {
+//     constructor() {
+//         this.arr = [];
+//     }
 
-    insert(ele) {
-        this.arr.push(ele);
-    }
+//     insert(ele) {
+//         this.arr.push(ele);
+//     }
 
-    remove() {
-        return this.arr.shift();
-    }
+//     remove() {
+//         return this.arr.shift();
+//     }
 
-    isEmpty() {
-        if (this.arr.length === 0) {
-            return console.log('this is empty');
-        }
-        else {
-            return console.log('this is not empty');
-        }
-    }
+//     isEmpty() {
+//         if (this.arr.length === 0) {
+//             return console.log('this is empty');
+//         }
+//         else {
+//             return console.log('this is not empty');
+//         }
+//     }
 
-    peak() {
-        return this.arr.length = [0];
-    }
+//     peak() {
+//         return this.arr[0];
+//     }
 
-    display() {
-        console.log(this.arr);
-    }
-}
+//     display() {
+//         console.log(this.arr);
+//     }
+// }
 
-let q = new Queue();
+// let q = new Queue();
 
-q.insert(10);
-q.insert(20);
-q.insert(30);
-q.insert(40);
-q.insert(50);
-q.display();
-q.remove();
-q.display();
-q.isEmpty();
-q.display();
-q.peak();
-q.display();
+// q.insert(10);
+// q.insert(20);
+// q.insert(30);
+// q.insert(40);
+// q.insert(50);
+// q.display();
+// q.remove();
+// q.display();
+
+// q.isEmpty();
+
+// console.log(q.peak());
 
 // output : 10,20,30,40,50
 //          20,30,40,50
 //          this is not empty [20,30,40,50]
-// 
+//          20
+
+
 /* =================================================================================== */
